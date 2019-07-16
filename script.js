5cb2e6bdf6a8e6aa18f0985a5a8f8c4d96d106b5
AndyS1988 <spaska.andrea@gmail.com>
2019-07-15 21:51:52 -0700

    tried to fix Cors error

--- script.js
+++ script.js
@@ -19,8 +19,9 @@ const db = knex({
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => { res.send('It is working!') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
