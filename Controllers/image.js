3fc3a180782200a558a2a1edd69592a904f40e54
AndyS1988 <spaska.andrea@gmail.com>
2019-07-25 18:06:02 -0700

    fixing env. variable

--- Controllers/image.js
+++ Controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: "c8989d1fe5f64039a87f682fc843a633"
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
